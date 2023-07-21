91e96c12a91f5a2aceaec075f8e9fe573a1cf047
Gabriel Silva <38063864+codingFive0@users.noreply.github.com>
2023-07-21 15:42:44 -0300

    Update Vouncher.php

--- exemples/Vouncher.php
+++ exemples/Vouncher.php
@@ -2,14 +2,6 @@
 
 use codingFive0\Certifier\Voucher;
 
-
-//CPF 15795276049
-//Nome do Usuário: 113D200624413D34 00015795276049
-//Senha: 98188dn1w38
-//
-//CPF 92595571001
-//Nome do Usuário: 113D2006244178DC 00092595571001
-//Senha: 3a3hv5i7bDC
 $inTest = true;
 
 if ($inTest) {
